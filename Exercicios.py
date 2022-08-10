6efa57deadae6529db2a963add8825ad37733ba0
SMARTG <smartg.guilherme@gmail.com>
2022-08-10 14:39:15 -0300

    Exercicios de 1 ao 6

---
+++ Exercicios.py
@@ -0,0 +1,37 @@
+#Exercicio 01
+
+#Ano = int(input("Em que ano você nasceu? "))
+#Ano_Atual = int(input("Em que ano nos estamos? "))
+#print(f"Você tem {Ano_Atual - Ano} anos.")
+
+#Exercicio 02
+
+#Dias = int(input("Quantos dias? "))
+#print(f"Você ira pagar {Dias * 100} reais.")
+
+#Exercicio 03
+
+#n = float(input("Qual a temperatura? "))
+#n2 = (n * 9/5) + 32
+#print(f"{n} Celsius é igual a {n2} Fahrenheit.")
+
+#Exercicio 04
+
+#n1 = int(input("Nota 01: "))
+#n2 = int(input("Nota 02: "))
+#n3 = int(input("Nota 03: "))
+#n4 = int(input("Nota 04: "))
+#soma = n1 + n2 + n3 + n4
+#print(f"A média é {soma/4}")
+
+#Exercicio 05
+
+#n = int(input("Quantos anos você tem: "))
+#print(f"Vo^Ce tem {n * 12} meses.")
+
+#Exercicio 06
+
+#n = str(input("Qual o produto: "))
+#n1 = float(input("Quantos quilos tem o produto: "))
+#n2 = float(input("Quanto é o quilo do produto: "))
+#print(f"Esse(a) {n} custa {n1 * n2}R$")
